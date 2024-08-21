f163d69b4304011ebd5afe98bd33ab45519d2279
CT7WD <63139550+CT7WD@users.noreply.github.com>
2024-08-21 10:34:14 -0400

    Update 'critics' table

--- src/db/migrations/20240821083621_createCriticsTable.js
+++ src/db/migrations/20240821083621_createCriticsTable.js
@@ -1,14 +1,32 @@
 
-exports.up = function(knex) {
-    return knex.schema.createTable("critics", (table) => {
-        table.increments("critic_id").primary();
-        table.string("preferred_name");
-        table.string("surname");
-        table.string("organization_name");
-        table.timestamps(true, true);
-      });
-};
+// exports.up = function(knex) {
+//     return knex.schema.createTable("critics", (table) => {
+//         table.increments("critic_id").primary();
+//         table.string("preferred_name");
+//         table.string("surname");
+//         table.string("organization_name");
+//         table.timestamps(true, true);
+//       });
+// };
 
-exports.down = function(knex) {
-    return knex.schema.dropTable("critics");
-};
+// exports.down = function(knex) {
+//     return knex.schema.dropTable("critics");
+// };
+
+exports.up = function(knex) {
+    return knex.schema.hasTable('critics').then(function(exists) {
+      if (!exists) {
+        return knex.schema.createTable('critics', (table) => {
+          table.increments('critic_id').primary();
+          table.string('preferred_name');
+          table.string('surname');
+          table.string('organization_name');
+          table.timestamps(true, true);
+        });
+      }
+    });
+  };
+  
+  exports.down = function(knex) {
+    return knex.schema.dropTableIfExists('critics');
+  };
