d24b68d4e03d04fe12febb0534103317e05ef9b1
CT7WD <63139550+CT7WD@users.noreply.github.com>
2024-09-12 07:40:14 -0400

    Updated knexfile.

--- knexfile.js
+++ knexfile.js
@@ -2,19 +2,19 @@ const path = require("path");
 
 require("dotenv").config();
 
-const {
-  NODE_ENV = "development",
-  DEVELOPMENT_DATABASE_URL,
-  PRODUCTION_DATABASE_URL,
-} = process.env;
-const URL =
-  NODE_ENV === "production"
-    ? PRODUCTION_DATABASE_URL
-    : DEVELOPMENT_DATABASE_URL;
-
 // const {
-//   URL = "postgresql://postgres@localhost/postgres",
+//   NODE_ENV = "development",
+//   DEVELOPMENT_DATABASE_URL,
+//   PRODUCTION_DATABASE_URL,
 // } = process.env;
+// const URL =
+//   NODE_ENV === "production"
+//     ? PRODUCTION_DATABASE_URL
+//     : DEVELOPMENT_DATABASE_URL;
+
+const {
+  URL = "postgresql://postgres@localhost/postgres",
+} = process.env;
 
 module.exports = {
   development: {
